54f96b4a39a69bd21d6a74b31994af5f2ecffa88
crackhd <5148375+crackhd@users.noreply.github.com>
2019-05-16 18:12:59 -0700

    Updates

---
+++ precache-manifest.385d0bf3e009a971580b41378bef488b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f473fe0ca00d76b915ebafa93c0266f",
+    "url": "/smart-tg-stats/index.html"
+  },
+  {
+    "revision": "d5f9678cb54e6c162e30",
+    "url": "/smart-tg-stats/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/smart-tg-stats/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "d5f9678cb54e6c162e30",
+    "url": "/smart-tg-stats/static/js/main.9cc139fa.chunk.js"
+  },
+  {
+    "revision": "7bd51fdb271cd3180d3b",
+    "url": "/smart-tg-stats/static/js/runtime~main.3b0d2042.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/smart-tg-stats/static/media/logo.5d5d9eef.svg"
+  }
+]);
