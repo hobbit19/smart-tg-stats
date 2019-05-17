fc2e26ce85a270e58b66a305d3e3dc3a79d35f07
crackhd <5148375+crackhd@users.noreply.github.com>
2019-05-16 17:11:16 -0700

    Add valid contract code

--- Readme.md
+++ Readme.md
@@ -1,1 +1,73 @@
-# Build
+**Smart Tg Stats** is an Ethereum-based robot that accepts requests and responds with
+statistic information regarding posts in Telegram Messenger.
+
+# Interface
+Here is the public contract interface:
+
+```sol
+
+  Request[]                       requests;
+
+  event                       NewRequest(uint);
+  event                       NewResponse(uint);
+
+  function AddRequest(bytes32 _channel, uint32 _postID)
+                        public payable returns (uint256 requestID);
+
+  // use
+  function GetLastRequestID() public view returns (uint256) ;
+
+  bool                        status;      // must be true if robot is online
+  uint256                     statusRenew; // timestamp; must be bigger than UTCNOW-24h
+```
+
+### Send Request
+Use method `AddRequest() payable`:
+
+- Channel username
+- Post ID
+
+Also records **Bid** - amount of ether sent with request, if any.
+Bid affects prioritization of the request and speed of updates.
+
+This amount of ether _may be returned_ to the caller (`msg.sender`) later - only, if there was no concurrency.
+In general cases, bid is non-refundable.
+Zero bids are fine but such requests may take infinity to be processed by the robot.
+
+After transaction mined on the node,
+immediately use getter `GetLastRequestID()` to get `uin256` handle of the created request
+and store it.
+
+### Response info
+The data behind `Request` struct has packing
+
+```sol
+enum ProcErr {
+  None,               // no error
+  UnknownError,       // there was technical difficulties
+  ChannelAccessError, // channel not found or bot blocked (temporarily or not)
+  PostAccessError     // invalid post ID
+}
+
+struct Request {
+    bytes32   channel;          /* Channel username */
+    uint32    postID;           /* Post ID */
+    uint256   bid;              /* Ether given for priority */
+    bool      willUpdate;       /* true: monitoring is ON for this request (updates are coming later) */
+    uint256   timeCreated;      /* Timestamp of the request */
+    uint256   timeFirst;        /* Timestamp of the first update of stats */
+    uint256   timeLast;         /* Timestamp of the latest update of stats */
+    uint32    lastSubscribers;  /* Stats: channel subscribes counter */
+    uint32    lastPostViews;    /* Stats: post views counter */
+    uint256   postTime;         /* Stats: UTC timestamp post created */
+    ProcErr   error;            /* Response: robot last error (if any) */
+}
+```
+
+### ABI
+```
+none
+```
+
+Mainnet address: `not deployed`
+Rinkeby address: `not deployed`
--- SmartTgStats.sol
+++ SmartTgStats.sol
@@ -1,98 +1,92 @@
 
+
 contract SmartTgStats {
 
-    address                     owner;
-    bool                        status;
-    uint256                     statusRenew;
-    uint256                     balance;
+  address                     owner;
+  bool                        status;
+  uint256                     statusRenew;
 
-    struct Request {
-        bytes32 channel;
-        uint32 postID;
-        uint256 bid;
-        bool willUpdate;
-        uint256 started;
-        uint256 updated;
-        uint32 lastSubscribers;
-        uint32 lastPostViews;
-        uint256 postTime;
-    }
+  enum ProcErr {
+    None,
+    UnknownError,
+    ChannelAccessError,
+    PostAccessError
+  }
 
-    Request[]                   requests;
-    mapping (bytes32 => Request) requestsMap;
+  struct Request {
+      string    channel;          /* Channel username */
+      uint32    postID;           /* Post ID */
+      uint256   bid;              /* Ether given for priority */
+      bool      willUpdate;       /* true: monitoring is ON for this request (updates are coming later) */
+      uint256   timeCreated;      /* Timestamp of the request */
+      uint256   timeFirst;        /* Timestamp of the first update of stats */
+      uint256   timeLast;         /* Timestamp of the latest update of stats */
+      uint32    lastSubscribers;  /* Stats: channel subscribes counter */
+      uint32    lastPostViews;    /* Stats: post views counter */
+      uint256   postTime;         /* Stats: UTC timestamp post created */
+      ProcErr   error;            /* Response: robot last error (if any) */
+  }
 
-    event                       NewRequest(uint);
-    event                       NewResponse(uint);
+  mapping (uint256 => Request)    public Requests;
+  uint256                         maxRequestID;
 
-    constructor(bool _isOn) public payable {
-        require(owner == address(0));
-        owner = msg.sender;
-        status = _isOn;
-        statusRenew = block.timestamp;
+  event                       NewRequest(uint);
+  event                       NewResponse(uint);
 
-        requests.push(requestsMap[""]); // prevent zero requestID
-    }
+  constructor(bool _isOn) public payable {
+      require(owner == address(0));
+      owner = msg.sender;
+      status = _isOn;
+      statusRenew = block.timestamp;
+  }
 
-    function LastRequestID() public view returns (uint256) {
-        return requests.length - 1;
-    }
-
-   function RenewStatus(bool _isOn) public {
-        require(owner != address(0));
-        require(owner == msg.sender);
-        status = _isOn;
-        statusRenew = block.timestamp;
-   }
-
-   function AddRequest(bytes32 _channel, uint32 _postID) public payable {
-       require(owner != address(0));
-
-       require(status == true);
-       require(block.timestamp - statusRenew >= 0);
-       require(block.timestamp - statusRenew < 1209600);
-
-       require(msg.value >= 0);
-       balance += msg.value;
-
-       Request memory request = Request(
-            _channel,
-            _postID,
-            msg.value,
-            true,
-            block.timestamp, 0,
-            0, 0, 0);
-       requests.push(request);
-
-       bytes32 hash = ""; // TODO
-       requestsMap[hash] = request;
-
-       emit NewRequest(requests.length - 1);
-   }
-
-  function AddResponse(uint _requestID, bool _willUpdate, uint32 _subscribers, uint32 _postViews, uint256 _postTime) public {
+  function CEORenewStatus(bool _isOn) public {
     require(owner != address(0));
     require(owner == msg.sender);
-    require(_requestID <= requests.length -1);
-    require(requests[_requestID].willUpdate == true);
+    status = _isOn;
+    statusRenew = block.timestamp;
+  }
 
-    requests[_requestID].willUpdate = _willUpdate;
-    requests[_requestID].lastSubscribers = _subscribers;
-    requests[_requestID].lastPostViews = _postViews;
-    requests[_requestID].postTime = _postTime;
+  function AddRequest(string memory _channel, uint32 _postID) public payable returns(uint256 requestID) {
+    require(owner != address(0));
 
-    bytes32 hash = ""; // TODO
-    requestsMap[hash] = requests[_requestID];
+    require(status == true);
+    require(block.timestamp - statusRenew >= 0);
+    require(block.timestamp - statusRenew < 1209600);
+
+    maxRequestID += 1;
+    requestID = maxRequestID;
+    Requests[requestID] = Request(
+          _channel,
+          _postID,
+          msg.value,
+          true,
+          block.timestamp, 0, 0,
+          0, 0, 0,
+          ProcErr.None
+    );
+
+    emit NewRequest(requestID);
+  }
+
+  function CEOAddResponse(uint _requestID, bool _willUpdate, uint32 _subscribers, uint32 _postViews, uint256 _postTime, ProcErr _error) public {
+    require(owner != address(0));
+    require(owner == msg.sender);
+    require(_requestID > 0 && _requestID <= maxRequestID);
+    require(Requests[_requestID].willUpdate == true);
+
+    Requests[_requestID].willUpdate = _willUpdate;
+    Requests[_requestID].lastSubscribers = _subscribers;
+    Requests[_requestID].lastPostViews = _postViews;
+    Requests[_requestID].postTime = _postTime;
+    Requests[_requestID].error = _error;
 
     emit NewResponse(_requestID);
   }
 
-  function Withdraw(address payable _to, uint256 _amount) public {
-
+  function CEOWithdraw(address payable _to, uint256 _amount) public {
     require(owner != address(0));
     require(msg.sender == owner);
-
-    require(balance >= _amount);
-    balance -= _amount;
 
     _to.transfer(_amount);
   }
